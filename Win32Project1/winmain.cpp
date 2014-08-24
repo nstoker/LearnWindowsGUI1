48124f42a2dc76eb11e4300b58e182f0cd03c02f
Neil Stoker <neil.stoker@gmail.com>
2014-08-24 16:14:57 +0100

    create  yellow circle

--- Win32Project1/winmain.cpp
+++ Win32Project1/winmain.cpp
@@ -1,75 +1,177 @@
-#ifndef UNICODE
-#define UNICODE
-#endif
-
 #include <windows.h>
+#include <d2d1.h>
+#pragma comment(lib, "d2d1")
 
-LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+#include "basewin.h"
 
-int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR pCmdLine, int nCmdShow)
+template <class T> void SafeRelease(T **ppT)
 {
-	// Register the window class
-	const wchar_t CLASS_NAME[] = L"Sample Window Class";
+	if (*ppT)
+	{
+		(*ppT)->Release();
+		*ppT = NULL;
+	}
+}
 
-	WNDCLASS wc = {};
+class MainWindow : public BaseWindow<MainWindow>
+{
+	ID2D1Factory            *pFactory;
+	ID2D1HwndRenderTarget   *pRenderTarget;
+	ID2D1SolidColorBrush    *pBrush;
+	D2D1_ELLIPSE            ellipse;
 
-	wc.lpfnWndProc = WindowProc;
-	wc.hInstance = hInstance;
-	wc.lpszClassName = CLASS_NAME;
+	void    CalculateLayout();
+	HRESULT CreateGraphicsResources();
+	void    DiscardGraphicsResources();
+	void    OnPaint();
+	void    Resize();
 
-	RegisterClass(&wc);
+public:
 
-	// Create the window
-	HWND hwnd = CreateWindowEx(
-		0,		// Optional window styles
-		CLASS_NAME,	// Window class
-		L"Learn to Program Windows",	// Window Text
-		WS_OVERLAPPEDWINDOW,			// Window style
+	MainWindow() : pFactory(NULL), pRenderTarget(NULL), pBrush(NULL)
+	{
+	}
 
-		// Size and positon
-		CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
+	PCWSTR  ClassName() const { return L"Circle Window Class"; }
+	LRESULT HandleMessage(UINT uMsg, WPARAM wParam, LPARAM lParam);
+};
 
-		NULL,	// Parent window
-		NULL,	// Menu
-		hInstance,	// Instance handle
-		NULL		// Additonal application data
-		);
+// Recalculate drawing layout when the size of the window changes.
 
-	if (hwnd == NULL)
+void MainWindow::CalculateLayout()
+{
+	if (pRenderTarget != NULL)
+	{
+		D2D1_SIZE_F size = pRenderTarget->GetSize();
+		const float x = size.width / 2;
+		const float y = size.height / 2;
+		const float radius = min(x, y);
+		ellipse = D2D1::Ellipse(D2D1::Point2F(x, y), radius, radius);
+	}
+}
+
+HRESULT MainWindow::CreateGraphicsResources()
+{
+	HRESULT hr = S_OK;
+	if (pRenderTarget == NULL)
+	{
+		RECT rc;
+		GetClientRect(m_hwnd, &rc);
+
+		D2D1_SIZE_U size = D2D1::SizeU(rc.right, rc.bottom);
+
+		hr = pFactory->CreateHwndRenderTarget(
+			D2D1::RenderTargetProperties(),
+			D2D1::HwndRenderTargetProperties(m_hwnd, size),
+			&pRenderTarget);
+
+		if (SUCCEEDED(hr))
+		{
+			const D2D1_COLOR_F color = D2D1::ColorF(1.0f, 1.0f, 0);
+			hr = pRenderTarget->CreateSolidColorBrush(color, &pBrush);
+
+			if (SUCCEEDED(hr))
+			{
+				CalculateLayout();
+			}
+		}
+	}
+	return hr;
+}
+
+void MainWindow::DiscardGraphicsResources()
+{
+	SafeRelease(&pRenderTarget);
+	SafeRelease(&pBrush);
+}
+
+void MainWindow::OnPaint()
+{
+	HRESULT hr = CreateGraphicsResources();
+	if (SUCCEEDED(hr))
+	{
+		PAINTSTRUCT ps;
+		BeginPaint(m_hwnd, &ps);
+
+		pRenderTarget->BeginDraw();
+
+		pRenderTarget->Clear(D2D1::ColorF(D2D1::ColorF::SkyBlue));
+		pRenderTarget->FillEllipse(ellipse, pBrush);
+
+		hr = pRenderTarget->EndDraw();
+		if (FAILED(hr) || hr == D2DERR_RECREATE_TARGET)
+		{
+			DiscardGraphicsResources();
+		}
+		EndPaint(m_hwnd, &ps);
+	}
+}
+
+void MainWindow::Resize()
+{
+	if (pRenderTarget != NULL)
+	{
+		RECT rc;
+		GetClientRect(m_hwnd, &rc);
+
+		D2D1_SIZE_U size = D2D1::SizeU(rc.right, rc.bottom);
+
+		pRenderTarget->Resize(size);
+		CalculateLayout();
+		InvalidateRect(m_hwnd, NULL, FALSE);
+	}
+}
+
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR, int nCmdShow)
+{
+	MainWindow win;
+
+	if (!win.Create(L"Circle", WS_OVERLAPPEDWINDOW))
 	{
 		return 0;
 	}
 
-	ShowWindow(hwnd, nCmdShow);
+	ShowWindow(win.Window(), nCmdShow);
 
-	// Run the message loop
+	// Run the message loop.
+
 	MSG msg = {};
 	while (GetMessage(&msg, NULL, 0, 0))
 	{
 		TranslateMessage(&msg);
 		DispatchMessage(&msg);
 	}
+
 	return 0;
 }
 
-LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+LRESULT MainWindow::HandleMessage(UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
 	switch (uMsg)
 	{
+	case WM_CREATE:
+		if (FAILED(D2D1CreateFactory(
+			D2D1_FACTORY_TYPE_SINGLE_THREADED, &pFactory)))
+		{
+			return -1;  // Fail CreateWindowEx.
+		}
+		return 0;
+
 	case WM_DESTROY:
+		DiscardGraphicsResources();
+		SafeRelease(&pFactory);
 		PostQuitMessage(0);
 		return 0;
 
 	case WM_PAINT:
-	{
-		PAINTSTRUCT ps;
-		HDC hdc = BeginPaint(hwnd, &ps);
+		OnPaint();
+		return 0;
 
-		FillRect(hdc, &ps.rcPaint, (HBRUSH)(COLOR_WINDOW + 1));
 
-		EndPaint(hwnd, &ps);
-	}
+	case WM_SIZE:
+		Resize();
 		return 0;
 	}
-	return DefWindowProc(hwnd, uMsg, wParam, lParam);
+	return DefWindowProc(m_hwnd, uMsg, wParam, lParam);
 }
+
